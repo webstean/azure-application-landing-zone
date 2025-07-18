35cc390224903e921551f738d1422b81de7cf8de
Andrew Webster <webstean@gmail.com>
2025-07-18 15:48:44 +1000

    Initial/Update main file

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
-## placeholder
-
-## work out some basic variables
-/*
-locals {
-  default_domain         = data.azuread_domains.default.domains[0].domain_name
-  zone_balancing_enabled = var.sku_name == "premium" || var.sku_name == "isolated" ? true : false
-  public_access_enabled  = var.data_pii == "yes" || var.data_pii == "yes" ? false : true
-}
-*/
+## check the input variables are axctually valid
 
 data "azurerm_resource_group" "this" {
   name = var.resource_group_name
 }
 
 data "azuread_group" "this" {
-  id = var.entra_group_id
+  object_id = startswith(var.entra_group_id, "/groups/") ? substr(var.entra_group_id, 8, -1) : var.entra_group_id
 }
 
+data "azuread_managed_identity" "this" {
+  name                = var.user_managed_id
+  resource_group_name = var.resource_group_name
+}
+
+/*
+data "azurerm_subnet" "private-endpoints" {
+  name                 = var.private_subnet_name
+  virtual_network_name = var.virtual_network_name
+  resource_group_name  = var.resource_group_name
+}
+*/
