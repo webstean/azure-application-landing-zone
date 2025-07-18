aa1029380a792b922b34214f9667698fcc93ec1f
Andrew Webster <webstean@gmail.com>
2025-07-18 16:07:37 +1000

    Initial/Update tags.tf file

--- tags.tf
+++ tags.tf
@@ -8,9 +8,6 @@ locals {
     costcentre  = var.cost_centre
     data_PII    = var.data_pii
     data_PHI    = var.data_phi
-    project     = var.owner
-    project     = var.project_name
-    application = var.application_name
     createdby   = "terraform"
     monitoring  = var.monitoring
     created     = formatdate("DD/MM/YYYY", timestamp())
