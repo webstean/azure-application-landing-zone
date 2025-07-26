a77f6f109e8dd0c0e039a5e7012218da399daece
Andrew Webster <webstean@gmail.com>
2025-07-26 22:51:21 +1000

    Initial/Update main file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ## landing-zone module
 ## check the input variables are actually valid
 
-## mandatory
+## must already exist, it cannot be created here
 data "azurerm_resource_group" "this" {
   name = var.resource_group_name
 }
@@ -15,10 +15,64 @@ data "azurerm_user_assigned_identity" "this" {
   resource_group_name = var.resource_group_name
 }
 
-/*
-data "azurerm_subnet" "private-endpoints" {
-  name                 = var.private_subnet_name
-  virtual_network_name = var.virtual_network_name
+module "lz-init" {
+  ## Creates the initial landing zone resources, such the Azure Key Vault, Identities, Backup & public and private DNS zone (only if var.private_endpoints_always_deployed == true)
+  source  = "./init/"
+
+  ## Naming 
+  landing_zone_name    = var.landing_zone_name
+  dns_zone_name        = var.dns_zone_name
+
+  ## SKUs and Sizes
+  sku_name             = var.sku_name
+  size_name            = var.size_name
+
+  ## Security
+  private_endpoints_always_deployed = var.private_endpoints_always_deployed
+
+  ## Location
+  subscription_id      = var.subscription_id
+  location_key         = var.location_key
   resource_group_name  = var.resource_group_name
+
+  ## Tags / Naming
+  owner                = var.owner
+  cost_centre          = var.cost_centre
+  monitoring           = var.monitoring
+  org_fullname         = var.org_fullname
+  org_shortname        = var.org_shortname
 }
-*/
+
+module "lz-setup" {
+  ## Creates the storage accounts
+  source  = "./setup/"
+
+  ## Naming 
+  landing_zone_name    = module.lz-init.landing_zone_name
+  dns_zone_name        = module.lz-init.dns_zone_name
+
+  ## Identity
+  entra_group_pag_id                      = module.lz-init.entra_group_pag_id
+  user_assigned_identity_graph_id         = module.lz-init.user_assigned_identity_graph_id
+  user_assigned_identity_landing_zone_id  = module.lz-init.user_assigned_identity_landing_zone_id
+
+  ## SKUs and Sizes
+  sku_name             = module.lz-init.landing_zone_name.sku_name
+  size_name            = module.lz-init.landing_zone_name.size_name
+
+  ## Security
+  private_endpoints_always_deployed = var.private_endpoints_always_deployed
+
+  ## Location
+  subscription_id      = module.lz-init.subscription_id
+  location_key         = module.lz-init.location_key
+  resource_group_name  = module.lz-init.resource_group_name
+
+  ## Tags / Naming
+  owner                = module.lz-init.owner
+  cost_centre          = module.lz-init.cost_centre
+  monitoring           = module.lz-init.monitoring
+  org_fullname         = module.lz-init.org_fullname
+  org_shortname        = module.lz-init.org_shortname
+}
+
