bb9f67c1f53e1d7a35f344879364db021292f10a
Andrew Webster <webstean@gmail.com>
2025-07-28 17:40:30 +1000

    Initial/Update init module tags file

--- modules/init/tags.tf
+++ modules/init/tags.tf
@@ -3,16 +3,16 @@ locals {
   ## Azure tags
   ## A maximum of 15 tags are allowed with keys no longer than 512 and values no longer than 256 characters.
   tags_default = {
-    type        = contains(["production", "live"], var.landing_zone_name) ? "permanent" : "temporary"
-    environment = var.landing_zone_name
-    costcentre  = var.cost_centre
-    data_PII    = var.data_pii
-    data_PHI    = var.data_phi
-    createdby   = "terraform"
-    monitoring  = var.monitoring
-    created     = formatdate("DD/MM/YYYY", timestamp())
+    type          = contains(["production", "live"], var.landing_zone_name) ? "permanent" : "temporary"
+    environment   = var.landing_zone_name
+    costcentre    = var.cost_centre
+    data_PII      = var.data_pii
+    data_PHI      = var.data_phi
+    createdby     = "terraform"
+    monitoring    = var.monitoring
+    created       = formatdate("DD/MM/YYYY", timestamp())
     owner_service = var.owner_service
-    owner_tech  = var.owner_tech
+    owner_tech    = var.owner_tech
   }
 }
 
